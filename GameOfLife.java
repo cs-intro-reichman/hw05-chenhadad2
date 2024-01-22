c9f4539cd43d763318d1429a29b6fbeebbcf57f1
chenhadad2 <chen.hadad@post.runi.ac.il>
2024-01-22 22:33:54 +0200

    2223

--- GameOfLife.java
+++ GameOfLife.java
@@ -95,9 +95,9 @@ public class GameOfLife {
 	// cell in the new board. Returns the new board.
 	public static int[][] evolve(int[][] board) {
 	int [][] newboard = new int[board.length][board[0].length];
-	for (int i = 0; i < board.length; i++){
+	for (int i = 1; i < board.length - 1; i++){
 
-	for (int j =0; j < board[0].length; j++){
+	for (int j = 1; j < board[0].length - 1; j++){
 		newboard[i][j] = cellValue(board, i, j);
 	}
 	}
